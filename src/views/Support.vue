4f1ad9efc3efb8d8608d3d61f2eb8f3f182e0510
Jazmin Segura <jazmin.novak@cog7.org>
2025-06-03 12:42:36 -0500

    Update Support page: switch to mailto link and remove input box

--- src/views/Support.vue
+++ src/views/Support.vue
@@ -1,25 +1,3 @@
-<style scoped>
-.support-logo {
-  min-height: 200px;
-
-  padding: 20px 0;
-
-  text-align: center;
-}
-
-.support-logo img {
-  max-width: 150px;
-}
-
-.list {
-  margin-bottom: 0;
-}
-
-.support-form {
-  padding: 16px;
-}
-</style>
-
 <template>
   <ion-page>
     <ion-header>
@@ -27,79 +5,50 @@
         <ion-buttons slot="start">
           <ion-menu-button></ion-menu-button>
         </ion-buttons>
-        <ion-title>Support</ion-title>
+        <ion-title>Report a Bug</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content>
+    <ion-content class="ion-padding">
       <div class="support-logo">
-        <img src="/assets/img/appicon.svg" alt="Ionic Logo" />
+        <img src="/assets/img/appicon.svg" alt="App Logo" />
       </div>
 
-      <div class="support-form">
-        <form @submit.prevent="submit" novalidate>
-          <ion-textarea
-            label="Enter your support message below"
-            labelPlacement="stacked"
-            fill="solid"
-            v-model="supportMessage"
-            name="supportQuestion"
-            rows="6"
-            :error-text="submitted && !supportMessage ? 'Support message is required' : ''"
-            required
-          ></ion-textarea>
-
-          <ion-row>
-            <ion-col>
-              <ion-button expand="block" type="submit">Submit</ion-button>
-            </ion-col>
-          </ion-row>
-        </form>
-      </div>
+      <ion-button
+        expand="block"
+        color="primary"
+        :href="mailtoLink"
+        target="_blank"
+      >
+        Email Support Team
+      </ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
 import {
   IonPage,
   IonHeader,
   IonToolbar,
   IonButtons,
   IonMenuButton,
-  IonButton,
-  IonContent,
   IonTitle,
-  IonTextarea,
-  IonRow,
-  IonCol,
-  toastController,
+  IonContent,
+  IonButton,
 } from '@ionic/vue';
 
-const supportMessage = ref('');
-const submitted = ref(false);
-
-onMounted(async () => {
-  const toast = await toastController.create({
-    message: 'This does not actually send a support request.',
-    duration: 3000,
-  });
-  await toast.present();
-});
-
-const submit = async () => {
-  submitted.value = true;
-
-  if (supportMessage.value) {
-    supportMessage.value = '';
-    submitted.value = false;
-
-    const toast = await toastController.create({
-      message: 'Your support request has been sent.',
-      duration: 3000,
-    });
-    await toast.present();
-  }
-};
+const mailtoLink = 'mailto:webmaster@cog7.org?subject=Bug%20Report&body=Please%20describe%20the%20issue%20you%20encountered:';
 </script>
+
+<style scoped>
+.support-logo {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 20px;
+}
+
+.support-logo img {
+  max-width: 150px;
+}
+</style>
